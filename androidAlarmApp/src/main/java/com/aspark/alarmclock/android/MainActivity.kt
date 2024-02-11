c24c1744dee6ce66d004e2a18e4a0edab3206912
Ashutosh Gupta <68068357+Ashutoshgupta22@users.noreply.github.com>
2024-02-11 17:22:05 +0530

    implement time picker dialog

--- androidAlarmApp/src/main/java/com/aspark/alarmclock/android/MainActivity.kt
+++ androidAlarmApp/src/main/java/com/aspark/alarmclock/android/MainActivity.kt
@@ -1,25 +1,61 @@
 package com.aspark.alarmclock.android
 
+import android.content.Context
+import android.graphics.Paint.Align
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.aspark.alarmclock.Greeting
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
+
+                var showTimer by remember { mutableStateOf(false) }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    Fab {
+                        showTimer = true
+                    }
+                    if (showTimer) ShowTimer(
+                        onConfirm = {
+                                    showTimer = false
+
+                    }, onDismiss = {
+                        showTimer = false
+                    })
                 }
             }
         }
@@ -27,14 +63,88 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+private fun Fab(onFabClick: () -> Unit) {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LargeFloatingActionButton(
+            onClick = { onFabClick() },
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = "")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ShowTimer(onConfirm: ()->Unit, onDismiss: ()->Unit) {
+
+    val calendar = Calendar.getInstance()
+    val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+    val currentMinute = calendar.get(Calendar.MINUTE)
+
+    val timeState = rememberTimePickerState(currentHour % 12, currentMinute, false)
+
+    Dialog(
+        onDismissRequest = { onDismiss() },
+        properties = DialogProperties(dismissOnBackPress = true, dismissOnClickOutside = true)
+    ) {
+        Card(
+            modifier = Modifier.wrapContentSize(),
+            shape = RoundedCornerShape(32.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Select time"
+                    )
+                }
+                TimePicker(
+                    state = timeState,
+                    colors = TimePickerDefaults.colors(Color.LightGray,
+                        timeSelectorSelectedContainerColor = Color.LightGray),
+                    layoutType = TimePickerLayoutType.Vertical
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 16.dp, bottom = 8.dp),
+                    horizontalArrangement = Arrangement.Absolute.Right
+                ) {
+                    TextButton(
+                        onClick = { onDismiss() }
+                    ) {
+                        Text(text = "Dismiss")
+                    }
+                    TextButton(
+                        onClick = {
+
+                            onConfirm()
+                        }
+                    ) {
+                        Text(text = "Confirm")
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        Fab {
+        }
     }
 }
