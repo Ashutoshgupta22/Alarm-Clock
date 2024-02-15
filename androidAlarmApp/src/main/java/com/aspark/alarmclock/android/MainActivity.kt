055f76c2b2e47d10532830b1f572f4a5523392af
Ashutosh Gupta <68068357+Ashutoshgupta22@users.noreply.github.com>
2024-02-16 01:05:23 +0530

    add alarm to list

--- androidAlarmApp/src/main/java/com/aspark/alarmclock/android/MainActivity.kt
+++ androidAlarmApp/src/main/java/com/aspark/alarmclock/android/MainActivity.kt
@@ -1,27 +1,30 @@
 package com.aspark.alarmclock.android
 
-import android.content.Context
-import android.graphics.Paint.Align
 import android.os.Bundle
-import android.os.SystemClock
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,9 +34,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import java.util.Calendar
+import java.util.TreeSet
+
+data class MyTime(val hour: Int, val minute: Int) : Comparable<MyTime> {
+    override fun compareTo(other: MyTime): Int {
+        return if (hour == other.hour) minute - other.minute
+        else hour - other.hour
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,23 +54,32 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
 
                 var showTimer by remember { mutableStateOf(false) }
+                val alarmList = remember { mutableStateOf(listOf<MyTime>()) }
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Fab {
-                        showTimer = true
-                    }
-                    if (showTimer) ShowTimer(
-                        onConfirm = { hour, minute ->
 
-                            Log.i("TAG", "ShowTimer: $hour")
-                            showTimer = false
+//                    Box(modifier = Modifier.fillMaxSize())
+//                    {
+                        AlarmList(alarmList = alarmList.value)
 
-                    }, onDismiss = {
-                        showTimer = false
-                    })
+                        Fab {
+                            showTimer = true
+                        }
+                        if (showTimer) ShowTimer(
+                            onConfirm = { hour, minute ->
+
+                                val time = MyTime(hour, minute)
+                                alarmList.value = alarmList.value.plus(time)
+                                showTimer = false
+
+                            }, onDismiss = {
+                                showTimer = false
+                            }
+                        )
+//                    }
                 }
             }
         }
@@ -83,7 +104,7 @@ private fun Fab(onFabClick: () -> Unit) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun ShowTimer(onConfirm: (Int, Int)->Unit, onDismiss: ()->Unit) {
+private fun ShowTimer(onConfirm: (Int, Int) -> Unit, onDismiss: () -> Unit) {
 
     val calendar = Calendar.getInstance()
     val currentHour = calendar.get(Calendar.HOUR)
@@ -114,8 +135,10 @@ private fun ShowTimer(onConfirm: (Int, Int)->Unit, onDismiss: ()->Unit) {
                 }
                 TimePicker(
                     state = timeState,
-                    colors = TimePickerDefaults.colors(Color.LightGray,
-                        timeSelectorSelectedContainerColor = Color.LightGray),
+                    colors = TimePickerDefaults.colors(
+                        Color.LightGray,
+                        timeSelectorSelectedContainerColor = Color.LightGray
+                    ),
                     layoutType = TimePickerLayoutType.Vertical
                 )
                 Row(
@@ -142,11 +165,73 @@ private fun ShowTimer(onConfirm: (Int, Int)->Unit, onDismiss: ()->Unit) {
     }
 }
 
+@Composable
+private fun AlarmList(alarmList: List<MyTime>) {
+
+    Log.i("TAG", "AlarmList: $alarmList")
+
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(alarmList.toSortedSet().toList()) {
+            TimeCard(time = it)
+//            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+}
+
+@Composable
+fun TimeCard(time: MyTime) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+        shape = RoundedCornerShape(12.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp)
+        ) {
+            val hour = time.hour % 12
+            val amPm = if (time.hour < 12) "am" else "pm"
+            val min = time.minute
+
+            Row(
+                modifier = Modifier.wrapContentSize()
+            ) {
+                Text(text = "$hour:$min", fontSize = 40.sp)
+                Row(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(text = amPm, fontSize = 18.sp)
+                }
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Switch(
+                    checked = true, onCheckedChange = {},
+                )
+            }
+        }
+    }
+}
+
+
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        Fab {
+
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+//            AlarmList(alarmList = mutableStateOf(listOf(MyTime(12, 34),
+//                MyTime(2, 3)))
+//            )
         }
     }
 }
